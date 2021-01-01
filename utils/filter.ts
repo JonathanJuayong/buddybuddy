050c4e2629dff2499bef55ebd1bdb9e7a52833ce
Jonathan Juayong <jonathan.juayong@gmail.com>
2021-01-01 14:35:24 +0800

    updated filters

--- utils/filter.ts
+++ utils/filter.ts
@@ -1,7 +1,7 @@
 export const filterProperties = (properties , filters) => {
   return properties
-    .filter(prop => prop.city.includes(filters.city))
-    .filter(prop => prop.monthlyRent >= filters.rent.min && prop.monthlyRent <= filters.rent.max)
+    .filter(prop => prop.city.includes(filters.rooms.city))
+    .filter(prop => prop.monthlyRent >= filters.rooms.rent.min && prop.monthlyRent <= filters.rooms.rent.max)
     .filter(prop => prop.type.includes(filters.rooms.type))
     .filter(prop => prop.bedrooms >= filters.rooms.bedrooms)
     .filter(prop => prop.bathrooms >= filters.rooms.bathrooms)
